f30c1cfb51b3da327ae66bd645585ca18e84c6c6
CycHuk <dimalanger@outlook.com>
2025-03-25 11:20:23 +0300

    Add database configuration and initialization scripts

---
+++ init.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS OllamaChat;
+USE OllamaChat;
+
+CREATE TABLE IF NOT EXISTS chats (
+    id CHAR(36) DEFAULT (UUID()) PRIMARY KEY,
+    title VARCHAR(255) NULL,
+    can_user_write BOOLEAN DEFAULT TRUE,
+    response_by ENUM('support', 'bot') DEFAULT 'bot',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_id CHAR(36) NOT NULL, 
+    writer ENUM('bot', 'support', 'user') NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
