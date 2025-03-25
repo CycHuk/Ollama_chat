e0cce79cc912697c5cf433f15769c43b66200430
CycHuk <dimalanger@outlook.com>
2025-03-25 11:45:02 +0300

    Set default title for chats table to 'Новый чат'

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ USE OllamaChat;
 
 CREATE TABLE IF NOT EXISTS chats (
     id CHAR(36) DEFAULT (UUID()) PRIMARY KEY,
-    title VARCHAR(255) NULL,
+    title VARCHAR(255) DEFAULT 'Новый чат',
     can_user_write BOOLEAN DEFAULT TRUE,
     response_by ENUM('support', 'bot') DEFAULT 'bot',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
