3dd711f99742f018ae167f65dc8faf52fc3a18e2
CycHuk <dimalanger@outlook.com>
2025-03-25 13:05:21 +0300

    Enhance database initialization script with UTF-8 support and enforce non-null title for chats

--- init.sql
+++ init.sql
@@ -1,13 +1,16 @@
-CREATE DATABASE IF NOT EXISTS OllamaChat;
+CREATE DATABASE IF NOT EXISTS OllamaChat
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
 USE OllamaChat;
 
 CREATE TABLE IF NOT EXISTS chats (
     id CHAR(36) DEFAULT (UUID()) PRIMARY KEY,
-    title VARCHAR(255) DEFAULT 'Новый чат',
+    title VARCHAR(255) NOT NULL,
     can_user_write BOOLEAN DEFAULT TRUE,
     response_by ENUM('support', 'bot') DEFAULT 'bot',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -15,6 +18,5 @@ CREATE TABLE IF NOT EXISTS messages (
     writer ENUM('bot', 'support', 'user') NOT NULL,
     message TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
     FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
