bc2f7a6cee2ad17f81ab3007024abe0b1706f3b5
CycHuk <dimalanger@outlook.com>
2025-03-26 13:53:25 +0300

    Add CORS middleware to FastAPI application for cross-origin requests

--- server/main.py
+++ server/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from routes import chat, message
 from sockets import message as ws_message, chat as ws_chat
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.include_router(chat.router)
 app.include_router(message.router)
 app.include_router(ws_message.router)
 app.include_router(ws_chat.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
